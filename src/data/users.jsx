dda988b07b5d9ccd19bbb2f6bd8814ea6f69fbd4
r4lax001 <relaxzrl1@gmail.com>
2025-11-16 02:23:15 +0700

    "delete sousce code by r4lax001 jaa"

--- src/data/users.jsx
+++
@@ -1,8 +0,0 @@
-const users = [
-  { user: "123", pass: "123", role: "user", token: "user" },
-];
-
-export function verifyUser(user, pass) {
-  const userFound = users.find((u) => u.user === user && u.pass === pass);
-  return userFound ? { role: userFound.role, token: userFound.token } : null;
-}
